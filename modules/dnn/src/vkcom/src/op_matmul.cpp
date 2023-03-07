927902669823089ffad4c5d5227f45243f8200dc
zihaomu <zihaomu@outlook.com>
2023-03-07 15:44:41 +0800

    try find the fastest code.

--- modules/dnn/src/vkcom/src/op_matmul.cpp
+++ modules/dnn/src/vkcom/src/op_matmul.cpp
@@ -10,8 +10,8 @@ namespace cv { namespace dnn { namespace vkcom {
 
 #ifdef HAVE_VULKAN
 
-#define KSTRIP_LEN 32
-#define BLOCK_SIZE 32
+#define KSTRIP_LEN 16
+#define BLOCK_SIZE 64
 
 #define MAX_COMPUTE_GFLOPS 10
 // TODO: query group count from vulkan device
@@ -65,7 +65,7 @@ bool OpMatMul::forward(std::vector<Tensor>& ins, std::vector<Tensor>& outs)
     std::vector<int> shape = {(int)param.size()};
     Tensor paramTensor = Tensor(reinterpret_cast<const char *>(param.data()), shape, kFormatInt32, VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT);
 
-    std::string key = "gemm_v11_spv";
+    std::string key = "gemm_v9_spv";
     destTypes = {
             VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, // input
             VK_DESCRIPTOR_TYPE_STORAGE_BUFFER, // weight
@@ -103,7 +103,6 @@ bool OpMatMul::forward(std::vector<Tensor>& ins, std::vector<Tensor>& outs)
 
     return true;
 }
-
 
 bool OpMatMul::computeGroupCount()
 {
