a0e898adc49e026b5257e6a2cfccc0d5a706f10d
zihaomu <zihaomu@outlook.com>
2023-03-14 16:38:54 +0800

    speed up vulkan dnn, and support ios and apple m1 chip.

--- modules/dnn/src/vkcom/src/internal.cpp
+++ modules/dnn/src/vkcom/src/internal.cpp
@@ -1,151 +1,151 @@
 // This file is part of OpenCV project.
 // It is subject to the license terms in the LICENSE file found in the top-level directory
 // of this distribution and at http://opencv.org/license.html.
-//
-// Copyright (C) 2018, Intel Corporation, all rights reserved.
-// Third party copyrights are property of their respective owners.
 
 #include "../../precomp.hpp"
-#include "common.hpp"
 #include "internal.hpp"
 
 namespace cv { namespace dnn { namespace vkcom {
-
 #ifdef HAVE_VULKAN
 
-std::vector<uint32_t> compile(const std::string& name,
-                              shaderc_shader_kind kind,
-                              const std::string& data)
+bool checkFormat(Format fmt)
 {
-    std::vector<uint32_t> result;
-#ifdef USE_SHADERC
-    shaderc::Compiler compiler;
-    shaderc::CompileOptions options;
-
-    // Like -DMY_DEFINE=1
-    //options.AddMacroDefinition("MY_DEFINE", "1");
-    options.SetGenerateDebugInfo();
-    options.SetTargetEnvironment(shaderc_target_env_vulkan, shaderc_env_version_vulkan_1_1);
-    shaderc::SpvCompilationResult module = compiler.CompileGlslToSpv(
-            data.c_str(), data.size(), kind, name.c_str(), options);
-
-    if (module.GetCompilationStatus() !=
-            shaderc_compilation_status_success) {
-        std::cerr << module.GetErrorMessage();
-    }
-
-    //std::vector<uint32_t> result(module.cbegin(), module.cend());
-    result.assign(module.cbegin(), module.cend());
-    return result;
-#else
-    assert(0);
-    return result;
-#endif
+    return (fmt > -1 && fmt < kFormatNum) ? true : false;
 }
 
-void bindTensor(VkDevice& device, Tensor& tensor, int binding, VkDescriptorSet descriptor_set)
+size_t elementSize(Format fmt)
 {
-    VkDescriptorBufferInfo desc_buffer_info = {};
-    desc_buffer_info.buffer = tensor.getBuffer()->getVkBuffer();
-    desc_buffer_info.offset = 0;
-    desc_buffer_info.range = tensor.size();
-
-    VkWriteDescriptorSet write_descriptor_set = {};
-    write_descriptor_set.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-    write_descriptor_set.dstSet = descriptor_set;
-    write_descriptor_set.dstBinding = binding;
-    write_descriptor_set.descriptorCount = 1;
-    write_descriptor_set.descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-    write_descriptor_set.pBufferInfo = &desc_buffer_info;
-
-    vkUpdateDescriptorSets(device, 1, &write_descriptor_set, 0, NULL);
-}
-
-void computeConvOutputShapeAndPadding(const PaddingMode& padding_mode,
-                                      int& padding_top, int& padding_left,
-                                      const int& in_h, const int& in_w,
-                                      const int& filter_h, const int& filter_w,
-                                      const int& dilation_h, const int& dilation_w,
-                                      const int& stride_h, const int& stride_w,
-                                      int& out_h, int& out_w)
-{
-    if (padding_mode == kPaddingModeValid)
+    if (fmt == kFormatFp32 || fmt == kFormatInt32)
     {
-        padding_top = 0;
-        padding_left = 0;
-        out_h = ceil((in_h - (filter_h - 1) * dilation_h) / stride_h);
-        out_w = ceil((in_w - (filter_w - 1) * dilation_w) / stride_w);
+        return 4;
     }
-    else if (padding_mode == kPaddingModeSame)
+    else if (fmt >= 0 && fmt < kFormatNum)
     {
-        padding_top = ((filter_h - 1) * dilation_h + 1) / 2;
-        padding_left = ((filter_w - 1) * dilation_w + 1) / 2;
-        out_h = ceil(in_h / stride_h);
-        out_w = ceil(in_w / stride_w);
-    }
-    else if (padding_mode == kPaddingModeCaffe)
-    {
-        const int filter_h_actual = dilation_h * (filter_h - 1) + 1;
-        const int filter_w_actual = dilation_w * (filter_w - 1) + 1;
-        out_h = (in_h + 2 * padding_top - filter_h_actual) / stride_h + 1;
-        out_w = (in_w + 2 * padding_left - filter_w_actual) / stride_w + 1;
+        CV_LOG_WARNING(NULL, format("Unsupported format %d", fmt));
     }
     else
     {
-        CV_Error(Error::StsError, format("Invalid padding mode:%d", padding_mode));
+        CV_Error(Error::StsError, format("Invalid format %d", fmt));
     }
+    return 0;
 }
 
-void computePoolOutputShape(const PaddingMode& padding_mode,
-        const int& padding_top, const int& padding_left,
-        const int& in_h, const int& in_w,
-        const int& filter_h, const int& filter_w,
-        const int& stride_h, const int& stride_w,
-        int& out_h, int& out_w)
+int shapeCount(const Shape& shape, int start, int end)
 {
-    if (padding_mode == kPaddingModeValid)
-    {
-        assert(padding_top == 0);
-        assert(padding_left == 0);
-        out_h = ceil((in_h - (filter_h - 1)) / stride_h);
-        out_w = ceil((in_h - (filter_w - 1)) / stride_w);
-    }
-    else if (padding_mode == kPaddingModeSame)
-    {
-        const int padding_top_ = filter_h / 2;
-        const int padding_left_ = filter_w / 2;
-        CV_Assert(padding_top == padding_top_);
-        CV_Assert(padding_left == padding_left_);
-        out_h = ceil(in_h / stride_h);
-        out_w = ceil(in_h / stride_w);
-    }
-    else if (padding_mode == kPaddingModeCaffe)
-    {
-        int out_h_ = static_cast<int>(ceil(static_cast<float>(
-                        in_h + 2 * padding_top - filter_h) / stride_h)) + 1;
-        int out_w_ = static_cast<int>(ceil(static_cast<float>(
-                        in_h + 2 * padding_left - filter_w) / stride_w)) + 1;
+    if (start == -1) start = 0;
+    if (end == -1) end = (int)shape.size();
 
-        if (padding_top || padding_left)
+    if (shape.empty())
+        return 0;
+
+    int elems = 1;
+    assert(start <= (int)shape.size() &&
+           end <= (int)shape.size() &&
+           start <= end);
+    for(int i = start; i < end; i++)
+    {
+        elems *= shape[i];
+    }
+    return elems;
+}
+
+void setXYZ(unsigned int* dstcode, const unsigned int* code, const size_t size,
+            uint32_t local_size_x, uint32_t local_size_y, uint32_t local_size_z, size_t* dstsize)
+{
+    uint32_t local_size_x_id = -1;
+    uint32_t local_size_y_id = -1;
+    uint32_t local_size_z_id = -1;
+    uint32_t gl_WorkGroupSize_id = -1;
+
+    const uint32_t* p = code;
+    uint32_t* dp = dstcode;
+
+    // skip magic version generator bound schema
+    memcpy(dp, p, 5 * sizeof(uint32_t));
+    p += 5;
+    dp += 5;
+
+    // foreach op
+    while ((const unsigned char*)p < (const unsigned char*)code + size)
+    {
+        uint32_t opcode = p[0];
+
+        uint16_t wordcount = opcode >> 16;
+        uint16_t op = opcode & 0xffff;
+
+        if (op == 16) // OpExecutionMode
         {
-            // If we have padding, ensure that the last pooling starts strictly
-            // inside the image (instead of at the padding); otherwise clip the last.
-            if ((out_h_ - 1) * stride_h >= in_h + padding_top) {
-                --out_h_;
+            uint32_t mode = p[2];
+            if (mode == 17) // LocalSize
+            {
+                memcpy(dp, p, wordcount * sizeof(uint32_t));
+
+                // set local_size_xyz
+                // 替换掉原始的 xyz
+                dp[3] = local_size_x;
+                dp[4] = local_size_y;
+                dp[5] = local_size_z;
+
+                p += wordcount;
+                dp += wordcount;
+                continue;
             }
-            if ((out_w - 1) * stride_h >= in_h + padding_left) {
-                --out_w;
-            }
-            assert((out_h_ - 1) * stride_h < in_h + padding_top);
-            assert((out_w_ - 1) * stride_w < in_h + padding_left);
         }
-        out_h = out_h_;
-        out_w = out_w_;
+        else if (op == 50) // OpSpecConstant
+        {
+            uint32_t id = p[2];
+            if (id == local_size_x_id || id == local_size_y_id || id == local_size_z_id)
+            {
+                p += wordcount;
+                continue;
+            }
+        }
+        else if (op == 51) // OpSpecConstantComposite
+        {
+            uint32_t id = p[2];
+            if (id == gl_WorkGroupSize_id)
+            {
+                if (wordcount == 6 && (p[3] == local_size_x_id || p[4] == local_size_y_id || p[5] == local_size_z_id))
+                {
+                    p += wordcount;
+                    continue;
+                }
+            }
+        }
+        else if (op == 71) // OpDecorate
+        {
+            uint32_t id = p[1];
+            uint32_t decoration = p[2];
+            if (decoration == 1) // SpecId
+            {
+                uint32_t specid = p[3];
+                if (specid == 233) local_size_x_id = id;
+                if (specid == 234) local_size_y_id = id;
+                if (specid == 235) local_size_z_id = id;
+                if (specid == 233 || specid == 234 || specid == 235)
+                {
+                    p += wordcount;
+                    continue;
+                }
+            }
+            else if (decoration == 11) // BuiltIn
+            {
+                uint32_t builtin = p[3];
+                if (builtin == 25) // WorkgroupSize
+                {
+                    gl_WorkGroupSize_id = id;
+                    p += wordcount;
+                    continue;
+                }
+            }
+        }
+
+        memcpy(dp, p, wordcount * sizeof(uint32_t));
+        p += wordcount;
+        dp += wordcount;
     }
-    else
-    {
-        CV_Error(Error::StsError, format("Invalid padding mode:%d", padding_mode));
-    }
+
+    *dstsize = (unsigned char*)dp - (unsigned char*)dstcode;
 }
 
 #endif // HAVE_VULKAN
